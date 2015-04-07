65ae5239584eb9dd90c02437f5256689a99f98e4
yxh <yangxuhui1992@163.com>
2015-04-07 10:47:58 +0800

    add StringtoInteger.cpp

---
+++ src/8.StringtoInteger(atoi)/StringtoInteger(atoi).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int atoi(string str) {
+        if (str.empty()) return 0;
+        auto pos = str.find_first_not_of(" ");
+        if (pos == string::npos) return 0;
+        str = str.substr(pos);
+        int ret = 0;
+        bool neg = false;
+        if (str[0] == '+' || str[0] == '-') {
+            if (str.size() == 1) return 0;
+            neg = (str[0] == '-');
+            str = str.substr(1);
+        }
+        for (auto c : str) {
+            if (!isdigit(c)) break;
+            int digit = c - '0';
+            if (neg) {
+                if (-ret < (INT_MIN + digit)/10)
+                    return INT_MIN;
+            } else {
+                if (ret > (INT_MAX - digit)/10)
+                    return INT_MAX;
+            }
+            ret = ret * 10 + digit;
+        }
+        return neg? -ret : ret;
+    }
+};
