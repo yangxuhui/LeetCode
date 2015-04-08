b3c595ef3aebda4fdaad72c47c644811196029b9
yxh <yangxuhui1992@163.com>
2015-04-08 21:04:37 +0800

    Updated ReverseInteger.cpp

--- src/7.ReverseInteger/ReverseInteger.cpp
+++ src/7.ReverseInteger/ReverseInteger.cpp
@@ -1,3 +1,9 @@
+// Source: https://leetcode.com/problems/reverse-integer/
+// 2015/4/7
+
+/******************************************************************************************
+// My Solution
+// Using string
 class Solution {
 public:
     int reverse(int x) {
@@ -19,4 +25,21 @@ public:
         }
         return (flag ? -ret : ret);
     }
-};
+}; //19ms
+***********************************************************************************************/
+
+// Reference the solution of haeol
+// (https://github.com/haoel/leetcode/blob/master/algorithms/reverseInteger/reverseInteger.cpp)
+class Solution {
+public:
+    int reverse(int x) {
+        int ret = 0;
+        while (x) {
+            if (ret > INT_MAX/10 || ret < INT_MIN/10)
+                return 0;
+            ret = ret * 10 + x % 10;
+            x /= 10;
+        }
+        return ret;
+    }
+}; // 14ms
