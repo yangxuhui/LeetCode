54e4b9db265356a059ae2582188b5d2ae5dafc6d
yxh <yangxuhui1992@163.com>
2015-04-11 15:24:54 +0800

    Updated IntegertoRoman.cpp, RomantoInteger.cpp

--- src/12.IntegertoRoman/IntegertoRoman.cpp
+++ src/12.IntegertoRoman/IntegertoRoman.cpp
@@ -1,40 +1,60 @@
 // Source: https://leetcode.com/problems/integer-to-roman/
 // 2015/4/10
 
+/**********************************************************************************************
+// Reference haoel
+// (https://github.com/haoel/leetcode/blob/master/algorithms/integerToRoman/integerToRoman.cpp)
+// A more elegant solution
+class Solution {
+public:
+    string intToRoman(int num) {
+        string symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int arr[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string ret;
+        for (int i = 0; num != 0; ++i) {
+            while (num >= arr[i]) {
+                ret += symbol[i];
+                num -= arr[i];
+            }
+        }
+        return ret;
+    }
+}; // 51ms
+************************************************************************************************/
 // My Solution
 class Solution {
 public:
     string intToRoman(int num) {
         string ret_str;
-	if (num >= 1000) {
-		ret_str.append(num / 1000, 'M');
-		num %= 1000; 
+		if (num >= 1000) {
+			ret_str.append(num / 1000, 'M');
+			num %= 1000; 
     	}
-	int len = 100;
+		int len = 100;
     	char carry[] = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
-	int i = 0, j = 1, k = 2;
+		int i = 0, j = 1, k = 2;
     	while (num) {
-		int digit = num/len;
-		if (digit) {
-			if (digit < 4)
-				ret_str.append(digit, carry[k]);
-			else if (digit == 4) {
-				ret_str.append(1, carry[k]);
-				ret_str.append(1, carry[j]);
-			} else if (digit == 5)
-				ret_str.append(1, carry[j]);
-			else if (digit == 9) {
-				ret_str.append(1, carry[k]);
-				ret_str.append(1, carry[i]);
-			} else {
-				ret_str.append(1, carry[j]);
-				ret_str.append(digit - 5, carry[k]);  
+			int digit = num/len;
+			if (digit) {
+				if (digit < 4)
+					ret_str.append(digit, carry[k]);
+				else if (digit == 4) {
+					ret_str.append(1, carry[k]);
+					ret_str.append(1, carry[j]);
+				} else if (digit == 5)
+					ret_str.append(1, carry[j]);
+				else if (digit == 9) {
+					ret_str.append(1, carry[k]);
+					ret_str.append(1, carry[i]);
+				} else {
+					ret_str.append(1, carry[j]);
+					ret_str.append(digit - 5, carry[k]);  
+				}
 			}
+			num %= len;
+			len /= 10;
+			i += 2; j += 2; k += 2;
 		}
-		num %= len;
-		len /= 10;
-		i += 2; j += 2; k += 2;
-	}
-	return ret_str;
+		return ret_str;
     }
 }; // 42ms
--- src/13.RomantoInteger/RomantoInteger.cpp
+++ src/13.RomantoInteger/RomantoInteger.cpp
@@ -5,29 +5,77 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char, int> m = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
-	int ret = 0;
-	int i = 0;
-	while (i < s.size() - 1) {
-		int j = i + 1;
-		if (s[j] == s[i]) {
-			do {
-			    ++j;
-			} while (j != s.size() && s[j] == s[i]);
-			ret += (m[s[i]] * (j - i));
-			i = j;
-			if (j == s.size()) break;
-		} else if (m[s[i]] < m[s[j]]) {
-			ret += (m[s[j]] - m[s[i]]);
-			i += 2;
-		} else {
+		map<char, int> m = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+		int ret = 0;
+		int i = 0;
+		while (i < s.size() - 1) {
+			int j = i + 1;
+			if (s[j] == s[i]) {
+				do {
+					++j;
+				} while (j != s.size() && s[j] == s[i]);
+				ret += (m[s[i]] * (j - i));
+				i = j;
+				if (j == s.size()) break;
+			} else if (m[s[i]] < m[s[j]]) {
+				ret += (m[s[j]] - m[s[i]]);
+				i += 2;
+			} else {
+				ret += m[s[i]];
+				++i;
+			} 
+		}
+		if (i != s.size()) {
 			ret += m[s[i]];
-			++i;
-		} 
+		}
+		return ret; 
 	}
-	if (i != s.size()) {
-		ret += m[s[i]];
-	}
-	return ret; 
-    }
 }; // 92ms
+
+/*********************************************************************************************
+// Reference haoel
+// (https://github.com/haoel/leetcode/blob/master/algorithms/romanToInteger/romanToInteger.cpp)
+// A more efficient solution
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ret = romanCharToInt(s[0]);
+        for (int i = 1; i != s.size(); ++i) {
+            int pre = romanCharToInt(s[i-1]), curr = romanCharToInt(s[i]);
+            if (pre < curr) 
+                ret = ret - pre + (curr - pre);
+            else
+                ret += curr;
+        }
+        return ret;
+    }
+private:
+    int romanCharToInt(char c) {
+        int d = 0;
+        switch (c) {
+            case 'I':
+               d = 1;
+               break;
+            case 'V':
+                d = 5;
+                break;
+            case 'X':
+                d = 10;
+                break;
+            case 'L':
+                d = 50;
+                break;
+            case 'C':
+                d = 100;
+                break;
+            case 'D':
+                d = 500;
+                break;
+            case 'M':
+                d = 1000;
+                break;
+        }
+        return d;
+    }
+}; // 66ms
+************************************************************************************************/
