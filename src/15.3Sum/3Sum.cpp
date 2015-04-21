59e887314eb54b42716f3411e515974b517bdaed
yxh <yangxuhui1992@163.com>
2015-04-21 09:49:15 +0800

    Updated 3Sum.cpp

--- src/15.3Sum/3Sum.cpp
+++ src/15.3Sum/3Sum.cpp
@@ -38,3 +38,41 @@ public:
         return ret;
     }
 }; // 90ms
+
+// Reference haoel
+// (https://github.com/haoel/leetcode/blob/master/algorithms/3Sum/3Sum.cpp)
+class Solution {
+public:
+    vector<vector<int> > threeSum(vector<int> &num) {
+        vector<vector<int> > ret;
+        int sz = num.size();
+        if (sz < 3) return ret;
+        sort(num.begin(), num.end());
+        for (int fst = 0; fst < sz - 2; ++fst) {
+            if (fst > 0 && num[fst] == num[fst-1]) continue;
+            int scd = fst + 1;
+            int trd = sz - 1;
+            while (scd < trd) {
+                int sum = num[fst] + num[scd] + num[trd];
+                if (sum < 0) {
+                    while (scd < sz && num[scd] == num[scd+1]) ++scd;
+                    ++scd;
+                } else if (sum == 0) {
+                    vector<int> tmp;
+                    tmp.push_back(num[fst]);
+                    tmp.push_back(num[scd]);
+                    tmp.push_back(num[trd]);
+                    ret.push_back(tmp);
+                    while (scd < sz && num[scd] == num[scd+1]) ++scd;
+                    while (trd > 0 && num[trd] == num[trd-1]) --trd;
+                    ++scd;
+                    --trd;
+                } else {
+                    while (trd > 0 && num[trd] == num[trd-1]) --trd;
+                    --trd;
+                }
+            }
+        }
+        return ret;
+    }
+}; // 71ms
