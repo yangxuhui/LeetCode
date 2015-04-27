5f5c3740c46b808e72dd0c6c9b701909ad0e500a
yxh <yangxuhui1992@163.com>
2015-04-27 09:12:17 +0800

    Update RemoveElement.cpp

--- src/27.RemoveElement/RemoveElement.cpp
+++ src/27.RemoveElement/RemoveElement.cpp
@@ -5,7 +5,6 @@
 class Solution {
 public:
     int removeElement(int A[], int n, int elem) {
-        if (n == 0) return 0;
         int len = 0;
         for (int i = 0; i < n; ++i)
             if (A[i] != elem)
