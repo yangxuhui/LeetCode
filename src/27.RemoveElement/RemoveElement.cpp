4d65b014cc128415abdd80bcac70209af6f74948
yxh <yangxuhui1992@163.com>
2015-04-27 09:04:41 +0800

    add RemoveElement.cpp

---
+++ src/27.RemoveElement/RemoveElement.cpp
@@ -0,0 +1,15 @@
+// Source: https://leetcode.com/problems/remove-element/
+// 2015/4/27
+
+// My Reference
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        if (n == 0) return 0;
+        int len = 0;
+        for (int i = 0; i < n; ++i)
+            if (A[i] != elem)
+                A[len++] = A[i];
+        return len;
+    }
+}; //7ms
