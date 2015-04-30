1cfed6106093ae0bdf230637dc040807e89dd7d8
yxh <yangxuhui1992@163.com>
2015-04-30 09:39:53 +0800

    Update SearchInsertPosition.cpp

--- src/35.SearchInsertPosition/SearchInsertPosition.cpp
+++ src/35.SearchInsertPosition/SearchInsertPosition.cpp
@@ -16,3 +16,19 @@ public:
         }
     }
 }; // 19ms
+
+// My Solution2
+// Using Binary Search(折半查找)
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] <target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+}; // 10ms
