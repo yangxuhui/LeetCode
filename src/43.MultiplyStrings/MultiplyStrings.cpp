d175a5798cff80b06b5a8ec8096170c07099591c
yxh <yangxuhui1992@163.com>
2015-05-07 21:19:47 +0800

    Update MultiplyStrings.cpp

--- src/43.MultiplyStrings/MultiplyStrings.cpp
+++ src/43.MultiplyStrings/MultiplyStrings.cpp
@@ -55,3 +55,24 @@ private:
         return ret;
     }
 }; // 38ms
+
+// Reference pezy
+// (https://github.com/pezy/LeetCode/tree/master/124.%20Multiply%20Strings)
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        string ret(num1.size() + num2.size(), '0');
+        for (int i = num1.size() - 1; i >= 0; --i) {
+            int carry = 0;
+            for (int j = num2.size() - 1; j >= 0; --j) {
+                int sum = (ret[i+j+1] - '0') + (num1[i] - '0') * (num2[j] - '0') + carry;
+                ret[i+j+1] = sum % 10 + '0';
+                carry = sum / 10;
+            }
+            ret[i] += carry;
+        }
+        auto pos = ret.find_first_not_of("0");
+        if (pos != string::npos) return ret.substr(pos);
+        else return "0";
+    }
+}; // 18ms
