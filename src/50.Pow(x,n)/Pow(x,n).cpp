cf0b387284738ab2ebae040993dfc28c0b076f9a
yxh <yangxuhui1992@163.com>
2015-05-08 15:26:34 +0800

    Update Pow(x,n).cpp

--- src/50.Pow(x,n)/Pow(x,n).cpp
+++ src/50.Pow(x,n)/Pow(x,n).cpp
@@ -18,3 +18,18 @@ private:
         else return x * positive_pow(x, n - 1);
     }
 }; // 9ms
+
+// Referernce pezy
+// (https://github.com/pezy/LeetCode/tree/master/082.%20Pow%28x%2C%20n%29)
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        else if (n&0x1) return n > 0 ? x * square(myPow(x, n/2)) : square(myPow(x, n/2)) / x;
+        else return square(myPow(x, n/2));
+    }
+private:
+    double square(double x) {
+        return x * x;
+    }
+}; // 6ms
