5e2a11d3cc9f2d0379105d9380c5deb642b7a81d
yxh <yangxuhui1992@163.com>
2015-05-22 17:14:30 +0800

    add My solution to RemoveDuplicatesfromSortedArray.cpp

--- src/26.RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.cpp
+++ src/26.RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.cpp
@@ -14,3 +14,16 @@ public:
         return size;
     }
 }; // 43ms
+
+// My Soulution
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int len = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            while (i < nums.size()-1 && nums[i] == nums[i+1]) ++i;
+            nums[len++] = nums[i];
+        }
+        return len;
+    }
+}; // 32ms
