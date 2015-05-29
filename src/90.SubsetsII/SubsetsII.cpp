3f0136decc7c7b22c7bdc77c9c30e61110297f63
yxh <yangxuhui1992@163.com>
2015-05-29 09:44:16 +0800

    add SubsetsII.cpp

---
+++ src/90.SubsetsII/SubsetsII.cpp
@@ -0,0 +1,40 @@
+// Source: https://leetcode.com/problems/subsets-ii/
+// 2015/5/29
+
+// My Solution
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ret;
+        ret.push_back(vector<int>());
+        sort(nums.begin(), nums.end());
+        int cnt = 1;
+        while (cnt <= nums.size()) {
+            vector<int> vec_helper;
+            for (int i = 0; i < cnt; ++i)
+                vec_helper.push_back(nums[i]);
+            ret.push_back(vec_helper);
+            int pos = next(vec_helper, nums);
+            while (pos != -1) {
+                int ix = 0;
+                for (int i = 0; i < nums.size(); ++i)
+                    if (vec_helper[pos] < nums[i]) {
+                        ix = i;
+                        break;
+                    }
+                for (int i = pos; i < vec_helper.size(); ++i, ++ix)
+                    vec_helper[i] = nums[ix];
+                ret.push_back(vec_helper);
+                pos = next(vec_helper, nums);
+            }
+            ++cnt;
+        }
+        return ret;
+    }
+private:
+    int next(vector<int> vec1, vector<int>& vec2) {
+        for (int i = vec1.size()-1, j = vec2.size()-1; i >= 0; --i, --j)
+            if (vec1[i] < vec2[j]) return i;
+        return -1;
+    }
+}; // 12ms
