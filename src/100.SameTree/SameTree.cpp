2d8502464715b01dcfc95a581f79bf1afa379153
yxh <yangxuhui1992@163.com>
2015-05-30 21:30:11 +0800

    add another elegant solution that reference pezy

--- src/100.SameTree/SameTree.cpp
+++ src/100.SameTree/SameTree.cpp
@@ -37,3 +37,14 @@ public:
             return false;
     }
 }; // 4ms
+
+// Reference pezy
+// (https://github.com/pezy/LeetCode/tree/master/003.%20Same%20Tree)
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p && q) return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        else if (!p && !q) return true;
+        else return false;
+    }
+}; // 4ms
