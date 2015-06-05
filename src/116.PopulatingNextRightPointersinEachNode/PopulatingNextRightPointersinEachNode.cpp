28e616d766b01583747c819f0278a1b60b7ad9dd
yxh <yangxuhui1992@163.com>
2015-06-05 15:36:44 +0800

    add PopulatingNextRightPointersinEachNode.cpp

---
+++ src/116.PopulatingNextRightPointersinEachNode/PopulatingNextRightPointersinEachNode.cpp
@@ -0,0 +1,29 @@
+// Source: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+// 2015/6/5
+
+// My Solution
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode *p = root;
+        while (p && p->left) {
+            TreeLinkNode *q = p;
+            while (q->next) {
+                q->left->next = q->right;
+                q->right->next = q->next->left;
+                q = q->next;
+            }
+            q->left->next = q->right;
+            q->right->next = NULL;
+            p = p->left;
+        }
+    }
+}; // 24ms
