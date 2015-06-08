aa2e47b7280bfc5904d16505eacb0416b0a55cdc
yxh <yangxuhui1992@163.com>
2015-06-08 21:47:09 +0800

    add recursive solution

--- src/116.PopulatingNextRightPointersinEachNode/PopulatingNextRightPointersinEachNode.cpp
+++ src/116.PopulatingNextRightPointersinEachNode/PopulatingNextRightPointersinEachNode.cpp
@@ -27,3 +27,17 @@ public:
         }
     }
 }; // 24ms
+
+// recurusive
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        if (root->left && root->right)
+            root->left->next = root->right;
+        if (root->next && root->right)
+            root->right->next = root->next->left;
+        connect(root->left);
+        connect(root->right);
+    }
+}; // 26ms
