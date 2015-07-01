caba6a03ec735c70d0a62e6052d21d2dd9868ea9
yxh <yangxuhui1992@163.com>
2015-07-01 20:10:50 +0800

    delete some unnessary statements

--- src/205.IsomorphicStrings/IsomorphicStrings.cpp
+++ src/205.IsomorphicStrings/IsomorphicStrings.cpp
@@ -6,10 +6,10 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         map<char, vector<int>> m1, m2;
-        for (size_t i = 0; i < s.size(); ++i)
+        for (size_t i = 0; i < s.size(); ++i) {
             m1[s[i]].push_back(i);
-        for (size_t i = 0; i < t.size(); ++i)
             m2[t[i]].push_back(i);
+        }
         map<int, set<vector<int>>> cnt_to_pos1, cnt_to_pos2;
         for (auto elem : m1)
             cnt_to_pos1[elem.second.size()].insert(elem.second);
